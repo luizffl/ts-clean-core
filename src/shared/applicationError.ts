4d8751c4ba5134c036bc112c64c06559fe160682
Luiz Leite <100888973+luizffl@users.noreply.github.com>
2024-11-29 19:28:40 -0300

    feat: :building_construction: add shared layer

---
+++ src/shared/applicationError.ts
@@ -0,0 +1,21 @@
+export class ApplicationError extends Error {
+  message = "Unexpected error.";
+
+  static buildFromError(error: Error): ApplicationError {
+    if (error instanceof ApplicationError) {
+      return error;
+    }
+
+    const applicationError = new ApplicationError();
+
+    if (error instanceof Error) {
+      applicationError.stack = error.stack;
+    }
+
+    return applicationError;
+  }
+}
+
+export class UserNotFoundError extends ApplicationError {
+  message = "User not found.";
+}
---
+++ src/shared/logLevel.ts
@@ -0,0 +1,10 @@
+export enum LogLevel {
+  FATAL = "fatal",
+  ERROR = "error",
+  WARN = "warn",
+  INFO = "info",
+  DEBUG = "debug",
+  TRACE = "trace",
+}
+
+export const DEFAULT_LOG_LEVEL: LogLevel = LogLevel.INFO;
